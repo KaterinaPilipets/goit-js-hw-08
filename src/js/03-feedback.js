7a0953d9c80b5b98d2bddeee54fd2a0e7ae26322
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-12-20 16:04:29 +0200

    done task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email;
+const message = form.elements.message;
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let formData = {};
+form.addEventListener('input', throttle(saveFormValue, 500));
+form.addEventListener('submit', submitForm);
+
+let data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+// console.log('data', data);
+email.value = data.email || '';
+message.value = data.message || '';
+
+function saveFormValue(event) {
+  if (data) {
+    formData = data;
+  }
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+function submitForm(event) {
+  console.log(data);
+  event.preventDefault();
+  localStorage.clear();
+  event.target.reset();
+}
