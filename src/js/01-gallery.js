8fd3dfd00eabd0e0f5bf5431767439d4fe93aaaf
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-12-20 09:16:31 +0200

    done task2 and task3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', onGalleryClick);
+gallery.insertAdjacentHTML('beforeend', createGaleryItems(galleryItems));
+
+let galleryLightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+}
+
+function createGaleryItems(galleryItemsArr) {
+  return galleryItemsArr
+    .map(
+      ({ preview, original, description }) => `
+      <li class= "gallery__item">
+          <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}"/>
+         </a></li>`
+    )
+    .join('');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(onTimeupdate), 1000);
+function onTimeupdate({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
