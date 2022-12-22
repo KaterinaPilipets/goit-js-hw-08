aa5a3f4392269eeaf5787b8696feb3ce3888c61c
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-12-22 22:30:12 +0200

    fix task2 throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,7 @@ import throttle from 'lodash/throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onTimeupdate), 1000);
+player.on('timeupdate', throttle(onTimeupdate, 1000));
 function onTimeupdate({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
